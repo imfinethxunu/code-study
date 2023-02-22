5528fa54087f8e4efe04048dd605c5008f35800d
imfinethxunu <haebin0424@naver.com>
2023-02-23 00:02:49 +0900

    second commit

--- function.cpp
+++ function.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 
+/*ppt. function1*/
+
 //void comp_num(int n1, int n2) {
 //	printf("n1 = %d n2 = %d", n1, n2);
 //}
@@ -64,3 +66,88 @@
 //	scanf("%d", num);
 //	fibo(num);
 //}
+
+/*return add*/
+//int main() {
+//	int i, num, sum;
+//	for(1)
+//}
+
+
+
+
+
+
+/*ppt. function2*/
+
+
+/* n1 n2 exchange*/
+//int n1, n2;
+//void exchange(int n1, int n2) {
+//	int tmp = 0;
+//	tmp = n1;
+//	n1 = n2;
+//	n2 = tmp;
+//	printf("n1 : %d n2 : %d", n1, n2);
+//}
+//void main() {
+//	printf("Enter the two integers : ");
+//	scanf("%d %d", &n1, &n2);
+//	printf("n1 : %d n2 : %d ", n1, n2);
+//	printf("After exchanging : ");
+//	exchange(n1, n2);
+//}
+
+/*HW4-3 squares of integers*/
+//교수님 코드 참고
+//int square(int x) {
+//	return x * x;
+//}
+//void main() {
+//	int x;
+//	for (x = 1; x <= 10; ++x)
+//		printf(" %d", square(x));
+//}
+
+/*HW4-4 sum each number*/
+//int sum(int num) {
+//	int n1, n2, n3, n4;
+//	int sum;
+//
+//	n1 = num / 1000;
+//	n2 = num / 100 - 10 * n1;
+//	n3 = num / 10 - 100 * n1 - 10 * n2;
+//	n4 = num % 10;
+//
+//	sum = n1 + n2 + n3 + n4;
+//
+//	return sum;
+//}
+//void main() {
+//	int num;
+//	printf("Enter the number : ");
+//	scanf("%d", &num);
+//	sum(num);
+//	printf("%d", sum(num));
+//}
+
+/*calculate maximum*/
+int maximum(int x, int y, int z) {
+	int max = x;
+
+	if (y > max) {
+		max = y;
+	}
+	if (z > max) {
+		max = z;
+	}
+	
+	return max;
+}
+void main() {
+	int x, y, z;
+	printf("Enter the three integers : ");
+	scanf("%d %d %d", &x, &y, &z);
+	maximum(x, y, z);
+	printf("result = %d", maximum(x, y, x));
+}
